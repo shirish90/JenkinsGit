522a1867343a82be505e339eeb5709c0f4644b22
Shirish Topiwala <shirish.topiwala@einfochips.com>
2016-12-21 14:05:52 +0530

    All Data

---
+++ JenkinsDemo2/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using JenkinsDemo2.Models;
+
+namespace JenkinsDemo2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            Home obj = new Home();
+            int A = obj.value1 = 2;
+            int B = obj.value2 = 3;
+
+            obj.output = A * B;
+
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        public ActionResult FormProcess(Home obj)
+        {
+            int A = obj.value1;
+            int B = obj.value2;
+
+            obj.output = A * B;
+
+
+            return View("Home/Index", obj);
+            //return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ JenkinsDemo2/Models/Home.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JenkinsDemo2.Models
+{
+    public class Home
+    {
+        public int value1 { get; set; }
+        public int value2 { get; set; }
+
+        public int output { get; set; }
+    }
+}
